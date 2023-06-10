3d2d22ebbf87d1ea9e784014e698de49a11c84d6
murtazajoo <jmtariq2003@gmail.com>
2023-06-10 18:36:47 +0530

    feat: github icon added (lib: react-icons)

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
 
 export default function Menu() {
   return (
@@ -18,6 +19,16 @@ export default function Menu() {
           <NavLink className="px-2 py-1" to="docs">
             Docs
           </NavLink>
+        </li>
+        <li className="font-saira">
+          <a
+            href="https://github.com/murtazajoo/openSourcer"
+            className="px-2 py-1"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub size={30} />
+          </a>
         </li>
       </ul>
     </nav>
