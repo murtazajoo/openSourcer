57f873b7e314163bf64cc9b03eb09489d5be1a0a
Akshay Jain <jainakshay1783@gmail.com>
2023-06-10 23:26:48 -0700

    feat: Add footer to website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="bg-slate-800 text-center mt-auto px-6 py-4 text-white">
-            <p>open source is awesome 💜</p>
+        <footer className="bg-slate-800 text-center mt-auto px-6 py-4 text-white font-saira">
+            <p className="text-6xl mb-8">open source is awesome 💜</p>
             <p>OpenSourcer is an open-source project. Feel free to contribute and help us to make this project better</p>
             <p className="text-slate-300 mt-2">Copyright © 2023 by openSourcer. Under 
                 <a href="https://github.com/murtazajoo/openSourcer/blob/main/LICENSE" 
